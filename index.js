58886d593668b99664dd788ad28dd7eed726d46b
Tarun Zacharias <tzacharias.97@gmail.com>
2025-04-14 14:28:07 +0000

    header_parser_microservice_fcc

--- index.js
+++ index.js
@@ -20,8 +20,16 @@ app.get('/', function (req, res) {
 });
 
 // your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
+app.get('/api/whoami',(req,res)=>{
+  const ipaddress = req.ip || req.headers['x-forwarded-for'];
+  const language = req.headers['accept-language'];
+  const software = req.headers['user-agent'];
+
+  res.json({
+    ipaddress,
+    language,
+    software
+  })
 });
 
 // listen for requests :)
